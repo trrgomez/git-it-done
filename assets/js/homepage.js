10a6af35d823796d09a030a21eed6765739e9a76
trgomez <88842062+trrgomez@users.noreply.github.com>
2021-12-02 15:51:55 -0600

    api was called when buttons were clicked

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,21 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons")
+
+var getFeaturedRepos = function(language) {
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+  fetch(apiUrl).then(function(response) {
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayRepos(data.items, language);
+      });
+    } else {
+      alert('Error: GitHub User Not Found');
+    }
+  });
+};
 
 var getUserRepos = function (user) {
   // format the github api url
@@ -85,4 +100,16 @@ var formSubmitHandler = function (event) {
   }
 };
 
+var buttonClickHandler = function(event){
+  var language = event.target.getAttribute("data-language")
+  // console.log(language)
+  if (language) {
+    getFeaturedRepos(language);
+  
+    // clear old content
+    repoContainerEl.textContent = "";
+  }
+}
+
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
